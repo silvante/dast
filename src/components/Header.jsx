ac049abc2dbfc13be5acd6404d0e807d5eb2180a
silvante <khamidov.ko@gmail.com>
2024-08-25 16:27:59 +0300

    init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <button className="border border-based rounded-xl py-1 px-2 text-2xl bg-white">
         <i className="bx bx-menu"></i>
       </button>
-      <input type="text" className="flex-1 border rounded-xl border-based px-5 bg-white" placeholder="search something..."/>
+      <input type="text" className="flex-1 border rounded-xl border-based px-5 bg-white outline-none" placeholder="Search Something..."/>
       <button className="base_bg text-white rounded-xl px-3 flex items-center">
         <i className="bx bx-plus text-2xl"></i> NEW
       </button>
